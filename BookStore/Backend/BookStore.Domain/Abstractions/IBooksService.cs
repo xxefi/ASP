74aced29d3c7c07381a6ab0173e33e414af217ee
Efi <134388584+xxefi@users.noreply.github.com>
2024-08-27 04:57:42 +0400

    Update IBooksService.cs

--- BookStore/Backend/BookStore.Domain/Abstractions/IBooksService.cs
+++ BookStore/Backend/BookStore.Domain/Abstractions/IBooksService.cs
@@ -1,12 +1,11 @@
-﻿using BookStore.Domain.Models;
+using BookStore.Domain.Models;
 
-namespace BookStore.Application.Services
+namespace BookStore.Application.Services;
+
+public interface IBooksService
 {
-    public interface IBooksService
-    {
-        Task<Guid> CreateBook(Book book);
-        Task<Guid> DeleteBook(Guid id);
-        Task<List<Book>> GetAllBooks();
-        Task<Guid> UpdateBook(Guid id, string title, string description, decimal price);
-    }
+    Task<Guid> CreateBook(Book book);
+    Task<Guid> DeleteBook(Guid id);
+    Task<List<Book>> GetAllBooks();
+    Task<Guid> UpdateBook(Guid id, string title, string description, decimal price);
 }
